a04bd4cd7bcd6a79cb62b33ec33f15a0a1e1aef9
陳信成 <m24927605@gmail.com>
2019-09-06 20:55:25 +0800

    insertion sort done

--- insertion_sort.js
+++ insertion_sort.js
@@ -1,16 +1,12 @@
 function insertionSort(arr){
-  console.log('origin array');
-  console.log(arr);
   for(let i=0;i<arr.length;i++){
     let pickIndex=i;
     const insertValue=arr[i];
+    //If the previous value is bigger than this value,we change both.
     while(i>=0 && arr[pickIndex-1] > insertValue){
       [arr[pickIndex],arr[pickIndex-1]]=[arr[pickIndex-1],arr[pickIndex]];
       pickIndex--;
-      console.log('pickIndex',pickIndex)
-      console.log(arr)
     }
-    arr[pickIndex]=insertValue;
   }
   return arr;
 }
