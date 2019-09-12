03477556cfc7a9468dd9ff661507a94b6e9107da
陳信成 <m24927605@gmail.com>
2019-09-12 22:26:31 +0800

    radix sort done

--- radix_sort.js
+++ radix_sort.js
@@ -1,41 +1,32 @@
 const {isSorted}=require('./utils');
 
-function radixSortLSD(array) {
-  const counter = [
-    []
-  ]; // Radix sort Array container to hold arrays from 0th digit to 9th digits
-  let max = 0,
-    mod = 10,
-    dev = 1; //max
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
+function getMaxNumberLength(arr) {
+  let max = 0;
+  for(let i=0;i<arr.length;i++){
+    if(max<arr[i]){
+      max=arr[i];
     }
   }
-  // determine the large item length
-  let maxDigitLength = (max + '').length;
-  console.log(`max + ''`,max + '')
-  for (let i = 0; i < maxDigitLength; i++, dev *= 10, mod *= 10) {
-    
-    for (let j = 0; j < array.length; j++) {
-      let bucket = Math.floor((array[j] % mod) / dev); // Formula to get the significant digit
-      if (counter[bucket] === undefined) {
-        counter[bucket] = [];
-      }
-      counter[bucket].push(array[j]);
+  return max.toString().length;
+}
+
+function getPosition(num, place){
+  return  Math.floor(Math.abs(num)/Math.pow(10,place))% 10;
+}
+
+function radixSortLSD(arr) {
+
+  const max = getMaxNumberLength(arr); // length of the max digit in the array
+
+  for (let i = 0; i < max; i++) {
+    let buckets = Array.from(Array(10), () => []);
+    for (let j = 0; j < arr.length; j++) {
+      buckets[getPosition(arr[j], i)].push(arr[j]); // pushing into buckets
     }
-    let pos = 0;
-    for (let j = 0; j < counter.length; j++) {
-      let value = undefined;
-      if (counter[j] !== undefined) {
-        while ((value = counter[j].shift()) !== undefined) {
-          array[pos++] = value;
-        }
-      }
-    }
+    arr = [].concat(...buckets);
   }
-  return array;
-};
+  return arr;
+}
 
 let arr = [10,15,1,60,5,100,25,50];
 console.log(radixSortLSD(arr));
