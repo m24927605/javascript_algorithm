4e5ac0e6a72ea0ffb2ade5f77f85c5b729ad2609
Michael <m24927605@gmail.com>
2019-09-27 14:51:22 +0800

    map done

--- map.js
+++ map.js
@@ -7,5 +7,15 @@ console.log(m.has(obj));
 console.log(m.size);
 console.log(m.keys());
 console.log(m.values());
+console.log(m.entries());
+m.forEach((value)=>{
+  console.log(value);
+});
 m.delete(obj);
 console.log(m);
+m.set({test:'1'},'old world');
+m.set({test:'2'},'normal world');
+console.log(m);
+m.clear();
+console.log(m);
+
